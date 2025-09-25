e18da8caa7277581b9b43272107e27f57fddfd65
Asaf Gabai <77976014+asafgabai@users.noreply.github.com>
2025-09-25 11:29:37 +0300

    Release Preparations (#42)
    Make AppTrust CLI an embedded plugin of the JFrog CLI

--- cli/cli.go
+++ cli/cli.go
@@ -9,53 +9,30 @@ import (
 	"github.com/jfrog/jfrog-cli-core/v2/plugins/components"
 )
 
-//func GetJfrogCliApptrustApp() components.App {
-//	appContext := app.NewAppContext()
-//	appEntity := components.CreateEmbeddedApp(
-//		"apptrust",
-//		nil,
-//		components.Namespace{
-//			Name:        "apptrust",
-//			Aliases:     []string{"at"},
-//			Description: "AppTrust commands.",
-//			Category:    "Command Namespaces",
-//			Commands: []components.Command{
-//				system.GetPingCommand(appContext),
-//				version.GetCreateAppVersionCommand(appContext),
-//				version.GetPromoteAppVersionCommand(appContext),
-//				version.GetRollbackAppVersionCommand(appContext),
-//				version.GetReleaseAppVersionCommand(appContext),
-//				version.GetDeleteAppVersionCommand(appContext),
-//				packagecmds.GetBindPackageCommand(appContext),
-//				packagecmds.GetUnbindPackageCommand(appContext),
-//				application.GetCreateAppCommand(appContext),
-//				application.GetUpdateAppCommand(appContext),
-//				application.GetDeleteAppCommand(appContext),
-//			},
-//		},
-//	)
-//	return appEntity
-//}
-
 func GetJfrogCliApptrustApp() components.App {
 	appContext := app.NewAppContext()
-	appEntity := components.CreateApp(
+	appEntity := components.CreateEmbeddedApp(
 		"apptrust",
-		"1.0.5",
-		"JFrog AppTrust CLI",
-		[]components.Command{
-			system.GetPingCommand(appContext),
-			version.GetCreateAppVersionCommand(appContext),
-			version.GetPromoteAppVersionCommand(appContext),
-			version.GetRollbackAppVersionCommand(appContext),
-			version.GetReleaseAppVersionCommand(appContext),
-			version.GetDeleteAppVersionCommand(appContext),
-			version.GetUpdateAppVersionCommand(appContext),
-			packagecmds.GetBindPackageCommand(appContext),
-			packagecmds.GetUnbindPackageCommand(appContext),
-			application.GetCreateAppCommand(appContext),
-			application.GetUpdateAppCommand(appContext),
-			application.GetDeleteAppCommand(appContext),
+		nil,
+		components.Namespace{
+			Name:        "apptrust",
+			Aliases:     []string{"at"},
+			Description: "AppTrust commands.",
+			Category:    "Command Namespaces",
+			Commands: []components.Command{
+				system.GetPingCommand(appContext),
+				version.GetCreateAppVersionCommand(appContext),
+				version.GetPromoteAppVersionCommand(appContext),
+				version.GetRollbackAppVersionCommand(appContext),
+				version.GetReleaseAppVersionCommand(appContext),
+				version.GetDeleteAppVersionCommand(appContext),
+				version.GetUpdateAppVersionCommand(appContext),
+				packagecmds.GetBindPackageCommand(appContext),
+				packagecmds.GetUnbindPackageCommand(appContext),
+				application.GetCreateAppCommand(appContext),
+				application.GetUpdateAppCommand(appContext),
+				application.GetDeleteAppCommand(appContext),
+			},
 		},
 	)
 	return appEntity
